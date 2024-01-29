8d08c867b2cd648a638ae30d4d2745953ca67fff
danielsotoortiz <danielsotoortiz@macbook-air.alumnado.educarm.net>
2024-01-29 12:04:43 +0100

    First version

--- src/ArraysAmpliacion/Ej5.java
+++ src/ArraysAmpliacion/Ej5.java
@@ -16,5 +16,31 @@ public class Ej5 {
     
         Scanner tec = new Scanner (System.in);
         
+        int cont=0;
+        char v []=new char [7];
+        
+        for (int i = 0; i < 7; i++) {
+            
+            v[i]=tec.nextLine().charAt(0);
+            
+            
+        }
+        
+        char v2;
+        
+        for (int i = 0; i < 7; i++) {
+            
+            v2=v[i];
+            
+            if (v2== 'a'|v2== 'e'|v2== 'i'|v2== 'o'|v2== 'u'){
+            
+                cont++;
+                
+            }
+            
+            
+        }
+        
+        System.out.println("Vocales="+cont);
     }
 }
