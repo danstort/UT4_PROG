d16cca357958143393782c9286e77dfb5fa5a7cc
danielsotoortiz <danielsotoortiz@macbook-air.alumnado.educarm.net>
2024-02-03 20:31:17 +0100

    First version

--- src/Ej3.java
+++ src/Ej3.java
@@ -1,19 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 
-/**
- *
- * @author danielsotoortiz
- */
+import java.util.Scanner;
+
 public class Ej3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner tec = new Scanner(System.in);
+
+        String p1;
+        int c1 = 0;
+        int w1 = 0;
+
+        p1 = tec.nextLine() + " ";  //Introduzco un espacio para cuando sea una sola palabra no se produzca un error en el for.
+
+        int n = 0;
+
+        for (int i = 0; n == 0; i++) {
+
+            if (p1.charAt(i) == ' ') {
+                n = i + 1;  //Pongo +1 para que no me coja un espacio en la primera palabra.
+            }
+        }
+
+        String fWord = p1.substring(0, n);
+        fWord=fWord.replace(" ", "");
+
+        if (fWord.length() == p1.length()) {
+            System.out.println(fWord);
+        } else {
+            String lWord = p1.substring(n);
+
+            System.out.println(lWord + fWord);
+        }
+
     }
-    
+
 }
